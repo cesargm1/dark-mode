281385d468988905fadd17861b5cdf35bce21bed
cesar <cg.gtav@gmail.com>
2023-06-20 19:10:10 +0200

    dark mode

--- César Garcia Muñoz/César Garcia Muñoz/javascript/index.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  const submenuTypeAnimals = document.getElementById('submenu_type_animals')
-
-  submenuTypeAnimals.classList.toggle('hidden')
-}
-
-function closeMenu() {
-  document.getElementById('submenu_type_animals').classList.add('hidden')
-}
