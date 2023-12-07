d7ddc73481762b69a38dbc2ab51804591199358f
cesar <cg.gtav@gmail.com>
2023-12-07 19:41:57 +0100

    No working title

---
+++ figura.js
@@ -0,0 +1,18 @@
+
+function ChangeColor(){
+    let titulo = document.querySelector(".titulo")
+    let element = document.body;
+    
+    element.classList.toggle("dark-mode")}
+
+
+
+
+//function ChangeColor() {
+
+//var element = document.body;
+
+ //element.classList.toggle("dark-mode");}
+
+
+ 
